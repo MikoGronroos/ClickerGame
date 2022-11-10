58239d5b446ebbf89be1293a4422f642ac900465
MikoGro <miko.gronroos@gmail.com>
2022-11-10 16:12:25 +0200

    Added counter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const[count, setCount] = useState(0)
+
   return (
     <>
       <h1>Name of the game</h1>
       <div id="container">
-        <div id="vertical-center">
-          <button id='clickButton'>Centered Button</button>
+        <div id="center">
+          <button onClick={() => setCount(count + 1)}>Centered Button</button>
+          <h1>{count}</h1>
         </div>
       </div>
     </>
   );
+
+
 }
 
 export default App;
